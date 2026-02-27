32d63614712b3440f7e7a63b7f8541c0141fe2a8
NahanRezende <nahhan.rezende@aluno.ufop.edu.br>
2026-02-27 15:12:45 -0300

    feat: limpando repositório para recomeçar tudo do zero

--- src/main/java/br/edu/ufop/web/sales/repository/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ufop.web.sales.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.ufop.web.sales.entity.Event;
-
-public interface EventRepository extends JpaRepository<Event, Long> {
-}
--- src/main/java/br/edu/ufop/web/sales/repository/SaleRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ufop.web.sales.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.ufop.web.sales.entity.Sale;
-
-public interface SaleRepository extends JpaRepository<Sale, Long> {
-}
